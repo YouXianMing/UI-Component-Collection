2adb1b2f27323603b1202487cc13982fb2f52e1c
YouXianMing <YouXianMing1987@126.com>
2016-07-25 10:17:51 +0800

    Add TouchEventView.h/.m

--- UITextFieldView/UITextFieldView/ViewController.m
+++ UITextFieldView/UITextFieldView/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "UITextFieldView.h"
 #import "UIView+SetRect.h"
+#import "TouchEventView.h"
 
 @interface ViewController ()
 
@@ -22,61 +23,81 @@
     
     [super viewDidLoad];
     
+    __weak typeof(self) wSelf = self;
     self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
     [self.view addSubview:self.scrollView];
     
+    // 电话号码
     {
-        UITextFieldView *fieldView       = [[UITextFieldView alloc] initWithFrame:CGRectMake(10, 300, Width - 20, 40)];
-        fieldView.textField.placeholder  = @"电话号码";
-        [self.scrollView addSubview:fieldView];
-        
-        [fieldView registerShouldChangeCharactersInRange:nil didBeginEditing:^(UITextFieldView *textFieldView) {
+        CGRect frame = CGRectMake(10, 300, Width - 20, 40);
+        [TouchEventView touchEventViewWithFrame:frame block:^(TouchEventView *touchEventView, ETouchEventViewEventType eventType) {
             
-            [UIView animateWithDuration:0.35f animations:^{
+            if (eventType == kTouchEventViewAddSubViews) {
                 
-                CGRect rect = [textFieldView rectFromView:self.scrollView];
-                self.scrollView.contentOffset = CGPointMake(0, rect.origin.y - 200);
-            }];
-            
-        } didEndEditing:^(UITextFieldView *textFieldView) {
-            
-            [UIView animateWithDuration:0.35f animations:^{
+                [wSelf.scrollView addSubview:touchEventView];
                 
-                self.scrollView.contentOffset = CGPointMake(0, 0);
-            }];
-            
-        } shouldReturn:^BOOL(UITextFieldView *textFieldView) {
-            
-            [textFieldView resignFirstResponder];
-            return YES;
+                UITextFieldView *fieldView       = [[UITextFieldView alloc] initWithFrame:touchEventView.bounds];
+                fieldView.textField.placeholder  = @"电话号码";
+                [touchEventView.contentView addSubview:fieldView];
+                
+                [fieldView registerShouldChangeCharactersInRange:nil didBeginEditing:^(UITextFieldView *textFieldView) {
+                    
+                    [UIView animateWithDuration:0.35f animations:^{
+                        
+                        CGRect rect = [textFieldView rectFromView:self.scrollView];
+                        wSelf.scrollView.contentOffset = CGPointMake(0, rect.origin.y - 200);
+                    }];
+                    
+                } didEndEditing:^(UITextFieldView *textFieldView) {
+                    
+                    [UIView animateWithDuration:0.35f animations:^{
+                        
+                        wSelf.scrollView.contentOffset = CGPointMake(0, 0);
+                    }];
+                    
+                } shouldReturn:^BOOL(UITextFieldView *textFieldView) {
+                    
+                    [textFieldView resignFirstResponder];
+                    return YES;
+                }];
+            }
         }];
     }
     
+    // 密码
     {
-        UITextFieldView *fieldView          = [[UITextFieldView alloc] initWithFrame:CGRectMake(10, 360, Width - 20, 40)];
-        fieldView.textField.placeholder     = @"密码";
-        fieldView.textField.secureTextEntry = YES;
-        [self.scrollView addSubview:fieldView];
-        
-        [fieldView registerShouldChangeCharactersInRange:nil didBeginEditing:^(UITextFieldView *textFieldView) {
+        CGRect frame = CGRectMake(10, 360, Width - 20, 40);
+        [TouchEventView touchEventViewWithFrame:frame block:^(TouchEventView *touchEventView, ETouchEventViewEventType eventType) {
             
-            [UIView animateWithDuration:0.35f animations:^{
+            if (eventType == kTouchEventViewAddSubViews) {
                 
-                CGRect rect = [textFieldView rectFromView:self.scrollView];
-                self.scrollView.contentOffset = CGPointMake(0, rect.origin.y - 200);
-            }];
-            
-        } didEndEditing:^(UITextFieldView *textFieldView) {
-            
-            [UIView animateWithDuration:0.35f animations:^{
+                [wSelf.scrollView addSubview:touchEventView];
+                UITextFieldView *fieldView          = [[UITextFieldView alloc] initWithFrame:touchEventView.bounds];
+                fieldView.textField.placeholder     = @"密码";
+                fieldView.textField.secureTextEntry = YES;
+                [touchEventView.contentView addSubview:fieldView];
                 
-                self.scrollView.contentOffset = CGPointMake(0, 0);
-            }];
-            
-        } shouldReturn:^BOOL(UITextFieldView *textFieldView) {
-            
-            [textFieldView resignFirstResponder];
-            return YES;
+                [fieldView registerShouldChangeCharactersInRange:nil didBeginEditing:^(UITextFieldView *textFieldView) {
+                    
+                    [UIView animateWithDuration:0.35f animations:^{
+                        
+                        CGRect rect = [textFieldView rectFromView:self.scrollView];
+                        wSelf.scrollView.contentOffset = CGPointMake(0, rect.origin.y - 200);
+                    }];
+                    
+                } didEndEditing:^(UITextFieldView *textFieldView) {
+                    
+                    [UIView animateWithDuration:0.35f animations:^{
+                        
+                        wSelf.scrollView.contentOffset = CGPointMake(0, 0);
+                    }];
+                    
+                } shouldReturn:^BOOL(UITextFieldView *textFieldView) {
+                    
+                    [textFieldView resignFirstResponder];
+                    return YES;
+                }];
+            }
         }];
     }
 }
